3951ba16e7af5cb58ebffc11573a9b1739da289b
Chris Rowles <c.rowles@sentrybay.com>
2024-07-21 07:18:37 +0100

    update folder structure

---
+++ app/network.py
@@ -0,0 +1,54 @@
+import psutil
+from asyncio import sleep
+
+
+async def network_data():
+    """
+    Asynchronously gathers network data and returns network statistics.
+
+    This function initializes a dictionary to store network statistics and
+    calls the `stats` function to populate it with data for a specified network interface.
+
+    Returns:
+        dict: A dictionary containing network statistics for a specified network interface.
+    """
+
+    network = dict()
+    network["stats"] = await stats()
+
+
+async def stats(inet="wlan0"):
+    """
+    Asynchronously calculates network statistics for a given network interface.
+
+    This function calculates the amount of data sent and received over a specified network
+    interface within a one-second interval.
+
+    Args:
+        inet (str): The network interface to monitor (default is "wlan0").
+
+    Returns:
+        dict: A dictionary containing the following network statistics:
+            - "interface": The network interface being monitored.
+            - "in": Amount of data received in MB over the interval.
+            - "out": Amount of data sent in MB over the interval.
+    """
+
+    net_stat = psutil.net_io_counters(pernic=True, nowrap=True)[inet]
+    net_in_1 = net_stat.bytes_recv
+    net_out_1 = net_stat.bytes_sent
+
+    await sleep(1)
+
+    net_stat = psutil.net_io_counters(pernic=True, nowrap=True)[inet]
+    net_in_2 = net_stat.bytes_recv
+    net_out_2 = net_stat.bytes_sent
+
+    net_in = round((net_in_2 - net_in_1) / 1024 / 1024, 3)
+    net_out = round((net_out_2 - net_out_1) / 1024 / 1024, 3)
+
+    return dict({
+        "interface": inet,
+        "in": net_in,
+        "out": net_out
+    })
--- manage.py
+++ manage.py
@@ -1,13 +1,17 @@
 import uuid
 import os.path
-# import ssl
-from tornado.httpserver import HTTPServer
-from tornado.options import define, options
-from tornado.ioloop import IOLoop
-from app.main import create_app
 
+from tornado.httpserver import HTTPServer
+from tornado.options import define, options, parse_command_line
+from tornado.ioloop import IOLoop
+from app import create_app
+
+
+# Define base directory
 base_dir = os.path.dirname(__file__)
 
+
+# Create application with specified settings
 app = create_app({
     'template_path': os.path.join(base_dir, 'public'),
     'static_path': os.path.join(base_dir, 'public'),
@@ -16,16 +20,22 @@ app = create_app({
     'debug': True
 })
 
+
+# Define command-line options
 define('port', default=4200, help='Listen port', type=int)
 
-# ssl_dir = str(base_dir + '/ssl')
-# ssl_ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-# ssl_ctx.load_cert_chain(os.path.join(ssl_dir, "host.crt"),
-#                         os.path.join(ssl_dir, "host.key"))
 
 def run():
+    """
+    Starts the tornado application server and listens on the specified address and port.
+    """
+
+    # Parse command line arguments
+    parse_command_line()
+
     http = HTTPServer(app)
     http.listen(options.port)
+
     print("Listening on http://localhost:" + str(options.port))
     IOLoop.current().start()
 
