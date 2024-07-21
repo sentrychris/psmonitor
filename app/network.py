df5375e76dd2d5ea5d9bef5b6362dc79cf265483
Chris Rowles <c.rowles@sentrybay.com>
2024-07-21 23:10:20 +0100

    network improvements

--- app/network.py
+++ app/network.py
@@ -1,60 +1,134 @@
+import asyncio
 import psutil
 
-from asyncio import sleep
 
-
-async def network_data():
+def network_data(avg_in_out=False):
     """
-    Asynchronously gathers network data and returns network statistics.
+    Gathers network data including interface details and statistics.
 
-    This function initializes a dictionary to store network statistics and
-    calls the `stats` function to populate it with data for a specified network interface.
+    This function retrieves the list of network interfaces and their statistics. Optionally,
+    it can also calculate the average network I/O statistics over a specified interval for each interface.
+
+    Args:
+        avg_in_out (bool): If True, calculates and includes average network I/O statistics over a specified interval.
+                           Defaults to False.
 
     Returns:
-        dict: A dictionary containing network statistics for a specified network interface.
+        dict: A dictionary containing network interfaces, their statistics, and optionally their average I/O statistics.
+              The structure is as follows:
+                - "interfaces": List of network interface names.
+                - "statistics": A dictionary of network statistics for each interface.
+                - "averages" (optional): A dictionary of average network I/O statistics for each interface (included only if avg_in_out is True).
     """
 
-    return {
-        "stats": await get_statistics()
+    data = {
+        "interfaces": get_interfaces(),
+        "statistics": get_statistics(),
     }
 
+    if avg_in_out is True:
+        interfaces = get_interfaces()
 
-async def get_statistics(inet="wlan0"):
+        averages = {}
+        for interface in interfaces:
+            averages[interface] = asyncio.run(get_avg_in_out(interface))
+        data["averages"] = averages
+
+    return data
+
+
+async def get_avg_in_out(inet="wlan0", interval=5, sample_rate=0.1):
     """
-    Asynchronously calculates network statistics for a given network interface.
-
-    This function calculates the amount of data sent and received over a specified network
-    interface within a one-second interval.
+    Asynchronously calculates average network statistics for a given network interface over a specified interval.
 
     Args:
         inet (str): The network interface to monitor (default is "wlan0").
+        interval (int): The duration in seconds over which to average the network statistics.
+        sample_rate (float): The sampling rate in seconds.
 
     Returns:
         dict: A dictionary containing the following network statistics:
             - "interface": The network interface being monitored.
-            - "in": Amount of data received in MB over the interval.
-            - "out": Amount of data sent in MB over the interval.
+            - "in": Average amount of data received in MB per second.
+            - "out": Average amount of data sent in MB per second.
     """
 
-    # Capture initial network stats
-    net_stat = psutil.net_io_counters(pernic=True, nowrap=True)[inet]
-    net_in_1 = net_stat.bytes_recv
-    net_out_1 = net_stat.bytes_sent
+    samples = int(interval / sample_rate)
+    total_net_in = 0
+    total_net_out = 0
 
-    # Wait for a second to capture the delta
-    await sleep(1)
+    for _ in range(samples):
+        net_stat = psutil.net_io_counters(pernic=True, nowrap=True)[inet]
+        net_in_1 = net_stat.bytes_recv
+        net_out_1 = net_stat.bytes_sent
 
-    # Capture network stats after 1 second
-    net_stat = psutil.net_io_counters(pernic=True, nowrap=True)[inet]
-    net_in_2 = net_stat.bytes_recv
-    net_out_2 = net_stat.bytes_sent
+        await asyncio.sleep(sample_rate)
 
-    # Calculate the data received and sent in MB
-    net_in = round((net_in_2 - net_in_1) / 1024 / 1024, 3)
-    net_out = round((net_out_2 - net_out_1) / 1024 / 1024, 3)
+        net_stat = psutil.net_io_counters(pernic=True, nowrap=True)[inet]
+        net_in_2 = net_stat.bytes_recv
+        net_out_2 = net_stat.bytes_sent
+
+        total_net_in += (net_in_2 - net_in_1)
+        total_net_out += (net_out_2 - net_out_1)
+
+    avg_net_in = round((total_net_in / interval) / 1024 / 1024, 3)
+    avg_net_out = round((total_net_out / interval) / 1024 / 1024, 3)
 
     return {
         "interface": inet,
-        "in": net_in,
-        "out": net_out
+        "in": avg_net_in,
+        "out": avg_net_out
     }
+
+
+def get_statistics():
+    """
+    Retrieves network statistics for all network interfaces on the system.
+
+    This function uses the `psutil` library to gather network I/O statistics for each
+    network interface and returns these statistics in a structured format.
+
+    Returns:
+        dict: A dictionary where each key is a network interface name, and the value is
+              another dictionary containing the following network statistics:
+                - 'mb_sent': Amount of data sent in megabytes.
+                - 'mb_received': Amount of data received in megabytes.
+                - 'pk_sent': Number of packets sent.
+                - 'pk_received': Number of packets received.
+                - 'error_in': Number of input errors.
+                - 'error_out': Number of output errors.
+                - 'dropout': Number of packets dropped.
+    """
+
+    interfaces = {}
+    for addr, stat in psutil.net_io_counters(pernic=True).items():
+        interfaces[addr] = {
+            'mb_sent': stat.bytes_sent / (1024.0 * 1024.0),
+            'mb_received': stat.bytes_recv / (1024.0 * 1024.0),
+            'pk_sent': stat.packets_sent,
+            'pk_received': stat.packets_recv,
+            'error_in': stat.errin,
+            'error_out': stat.errout,
+            'dropout': stat.dropout,
+        }
+
+    return interfaces
+
+
+def get_interfaces():
+    """
+    Retrieves a list of network interface names on the system.
+
+    This function uses the `psutil` library to get network interface addresses and returns
+    the names of all network interfaces.
+
+    Returns:
+        list: A list of network interface names.
+
+    Example:
+        ["eth0", "wlan0", "lo"]
+    """
+
+    interfaces = psutil.net_if_addrs()
+
+    return [*interfaces.keys()]
--- app/wifi.py
+++ app/wifi.py
@@ -1,18 +1,21 @@
-import netifaces
 import re
 import subprocess
 
 
-def parse_wifi_info():
+def get_wifi_data():
+    """
+    Parses Wi-Fi information by scanning available Wi-Fi networks using the iwlist command.
+
+    Returns:
+        dict: A dictionary containing parsed Wi-Fi information.
+    """
+
     cells = [[]]
     info = {}
 
-    interface = get_wifi_interface()
-    print(str(" interface: ") + interface)
-
     try:
         interface = get_wifi_interface()
-        print(str(" interface: ") + interface)
+        print(interface)
         proc = subprocess.Popen(["iwlist", interface, "scan"], stdout=subprocess.PIPE, universal_newlines=True)
         out, err = proc.communicate()
 
@@ -34,14 +37,33 @@ def parse_wifi_info():
 
 
 def get_wifi_interface():
-    interfaces = netifaces.interfaces()
-    for interface in interfaces:
-        if interface.startswith("wl") or "wireless" in interface:
-            return interface
-        return None
+    """
+    Gets the wi-fi interface using the iw command.
+
+    Returns:
+        str: the name of the wi-fi interface.
+    """
+
+    proc = subprocess.Popen(['iw', 'dev'], stdout=subprocess.PIPE, universal_newlines=True)
+    out, err = proc.communicate()
+
+    interface = 'wlan0'
+    for line in out.split("\n"):
+        addr = match(line, "Interface ")
+        if addr is not None:
+            interface = addr
+
+    return interface
 
 
 def run_wifi_speedtest():
+    """
+    Runs a wi-fi speed test using speedtes-cli and parses the results.
+
+    Returns:
+        dict: A dictionary containing ping, download, and upload speeds.
+    """
+
     speedtest = subprocess.Popen('speedtest-cli --simple', shell=True,
                                  stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 
@@ -63,23 +85,73 @@ def run_wifi_speedtest():
 
 
 def get_name(cell):
+    """
+    Extracts the ESSID (name) from a cell.
+
+    Args:
+        cell (list): the cell information.
+
+    Returns:
+        str: The ESSID.
+    """
+
     return matching_line(cell, "ESSID:")[1:-1]
 
 
 def get_quality(cell):
+    """
+    Extracts the quality from a cell and converts it to a percentage.
+
+    Args:
+        cell (list): the cell information.
+
+    Returns:
+        str: The quality of the cell as a percentage.
+    """
+
     quality = matching_line(cell, "Quality=").split()[0].split('/')
     return str(int(round(float(quality[0]) / float(quality[1]) * 100))).rjust(3)
 
 
 def get_channel(cell):
+    """
+    Extracts the channel from a cell.
+
+    Args:
+        cell (list): the cell information.
+
+    Returns:
+        str: The wi-fi channel.
+    """
+
     return matching_line(cell, "Channel:")
 
 
 def get_signal_level(cell):
+    """
+    Extracts the signal level from a cell.
+
+    Args:
+        cell (list): The cell information.
+
+    Returns:
+        str: The signal level of the cell.
+    """
+
     return matching_line(cell, "Quality=").split("Signal level=")[1]
 
 
 def get_encryption(cell):
+    """
+    Extracts the encryption type from a cell.
+
+    Args:
+        cell (list): The cell information.
+
+    Returns:
+        str: The encryption type of the cell.
+    """
+
     enc = ""
     if matching_line(cell, "Encryption key:") == "off":
         enc = "Open"
@@ -96,10 +168,31 @@ def get_encryption(cell):
 
 
 def get_address(cell):
+    """
+    Extracts the address from a cell.
+
+    Args:
+        cell (list): The cell information.
+
+    Returns:
+        str: The address of the cell.
+    """
+
     return matching_line(cell, "Address: ")
 
 
 def matching_line(lines, keyword):
+    """
+    Finds the first line that matches a given keyword.
+
+    Args:
+        lines (list): The lines to search through.
+        keyword (str): The keyword to search for.
+
+    Returns:
+        str: The matching line, or None if no match is found.
+    """
+
     for line in lines:
         matching = match(line, keyword)
         if matching is not None:
@@ -108,6 +201,17 @@ def matching_line(lines, keyword):
 
 
 def match(line, keyword):
+    """
+    Checks if a line starts with a given keyword.
+
+    Args:
+        line (str): The line to check.
+        keyword (str): The keyword to check for.
+
+    Returns:
+        str: The line with the keyword stripped, or None if no match.
+    """
+
     line = line.lstrip()
     length = len(keyword)
     if line[:length] == keyword:
@@ -117,6 +221,16 @@ def match(line, keyword):
 
 
 def parse_cell(cell):
+    """
+    Parses a cell's information into a dictionary.
+
+    Args:
+        cell (list): The cell information.
+
+    Returns:
+        dict: A dictionary containing parsed cell information.
+    """
+
     rules = {
         "name": get_name,
         "quality": get_quality,
@@ -132,5 +246,3 @@ def parse_cell(cell):
         parsed_cell.update({key: rule(cell)})
 
     return parsed_cell
-
-print(parse_wifi_info())
