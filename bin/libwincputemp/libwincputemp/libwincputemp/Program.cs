2f0f179955899a0f9619a81d79d0f78fb2456d8c
Chris Rowles <christopher.rowles@outlook.com>
2024-07-25 13:37:30 +0100

    libwincputemp

---
+++ bin/libwincputemp/libwincputemp/libwincputemp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using LibreHardwareMonitor.Hardware;
+
+namespace LibWinCPUTemp
+{
+    /**
+     * A tiny CLI program for obtaining CPU temperature readings using LibreOpenHardwareMonitor's
+     * LibreOpenHardwareMonitorLib.dll. PSMonitor depends on this executable to provide a CPU temp
+     * value on Windows.
+     */
+    class Program
+    {
+        static readonly Computer computer = new Computer()
+        {
+            IsCpuEnabled = true,
+        };
+
+        static void GetCPUTemp()
+        {
+            foreach (var hardware in computer.Hardware)
+            {
+                hardware.Update();
+            }
+
+            var cpuSensor = GetCpuPackageTemperatureSensor();
+
+            if (cpuSensor != null)
+            {
+                Console.WriteLine(cpuSensor.Value.GetValueOrDefault());
+            } else
+            {
+                Console.WriteLine("N/A");
+            }
+        }
+
+        private static ISensor GetCpuPackageTemperatureSensor()
+        {
+            return computer.Hardware
+                .Where(i => i.HardwareType == HardwareType.Cpu)
+                .SelectMany(s => s.Sensors)
+                .FirstOrDefault(s => {
+                    return s.SensorType == SensorType.Temperature && s.Name.Contains("CPU Package");
+                });
+        }
+
+        static void Main()
+        {
+            computer.Open();
+            GetCPUTemp();
+            Environment.Exit(0);
+        }
+    }
+}
